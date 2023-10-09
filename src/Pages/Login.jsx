172805f7f5859ec6b1be944b1f7a25c9607b02ba
stevenennis <steven.otieno1@gmail.com>
2023-10-09 17:06:29 +0300

    :ft(login): add loader

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
-import LoginComponent from '../components/LoginComponent'
+import React, { useEffect, useState } from "react";
+import LoginComponent from "../components/LoginComponent";
+import { onAuthStateChanged } from "firebase/auth";
+import { authentication } from "../firebasConfig";
+import { useNavigate } from "react-router-dom";
+import Loader from "../components/common/Loader";
 
 export default function Login() {
+  let navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    onAuthStateChanged(authentication, (res) => {
+      if (res?.accessToken) {
+        navigate("/home");
+        setLoading(false);
+      } else {
+        navigate("/");
+        setLoading(false);
+      }
+    });
+  }, []);
   return (
-    <LoginComponent />
-  )
+    <>
+      {loading && <Loader />}
+      <LoginComponent />
+    </>
+  );
 }
