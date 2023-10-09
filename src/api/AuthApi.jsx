2209a651b9a05db9bb13fcacf267759f05e9f245
stevenennis <steven.otieno1@gmail.com>
2023-10-09 15:38:48 +0300

    ft(login): add google auth method

--- src/api/AuthApi.jsx
+++ src/api/AuthApi.jsx
@@ -1,4 +1,9 @@
-import { signInWithEmailAndPassword, createUserWithEmailAndPassword } from "firebase/auth";
+import {
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  GoogleAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
 import { authentication } from "../firebasConfig";
 
 export const LoginApi = (email, password) => {
@@ -11,10 +16,24 @@ export const LoginApi = (email, password) => {
 };
 
 export const RegisterApi = (email, password) => {
-    try {
-      let response = createUserWithEmailAndPassword(authentication, email, password);
-      return response;
-    } catch (error) {
-      return error;
-    }
-  };
+  try {
+    let response = createUserWithEmailAndPassword(
+      authentication,
+      email,
+      password
+    );
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
+export const GoogleSignInAPI = () => {
+  try {
+    let googleProvider = new GoogleAuthProvider();
+    let res = signInWithPopup(authentication, googleProvider);
+    return res;
+  } catch (err) {
+    return err;
+  }
+};
