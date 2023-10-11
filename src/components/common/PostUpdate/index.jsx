ec9d0790738c6c5e339648aba7ad76571d4ea9ce
stevenennis <steven.otieno1@gmail.com>
2023-10-11 21:25:43 +0300

    ft(post): add handle submit func

--- src/components/common/PostUpdate/index.jsx
+++ src/components/common/PostUpdate/index.jsx
@@ -1,13 +1,28 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import "./index.scss";
 import ModalComponent from "../Modal";
 
 export default function PostStatus() {
-    const [open, setOpen] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [status, setStatus] = useState(false);
+
+  //   input dispatch to firebase
+  const handleSubmit = () => {};
+
   return (
     <div className="post-status-main">
-      <div className="post-status"><button onClick={()=> setOpen(true)} className="open-post-modal">Start a post</button></div>
-      <ModalComponent open={open} setOpen={setOpen} />
+      <div className="post-status">
+        <button onClick={() => setOpen(true)} className="open-post-modal">
+          Start a post
+        </button>
+      </div>
+      <ModalComponent
+        open={open}
+        setOpen={setOpen}
+        status={status}
+        setStatus={setStatus}
+        handleSubmit={handleSubmit}
+      />
     </div>
   );
 }
