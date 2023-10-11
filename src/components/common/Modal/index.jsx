f550dab034d34c9356e944a6dae23b2d1f2eeceb
stevenennis <steven.otieno1@gmail.com>
2023-10-11 20:34:23 +0300

    ft(post component): add pop up modal

---
+++ src/components/common/Modal/index.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { Modal } from "antd";
+import './index.scss'
+
+export default function ModalComponent({open, setOpen}) {
+  
+  const showModal = () => {
+    setOpen(true);
+  };
+  const handleOk = (e) => {
+    console.log(e);
+    setOpen(false);
+  };
+  const handleCancel = (e) => {
+    console.log(e);
+    setOpen(false);
+  };
+  return (
+    <>
+      <Modal
+        title="Basic Modal"
+        open={open}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        okButtonProps={{
+          disabled: true,
+        }}
+        cancelButtonProps={{
+          disabled: true,
+        }}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+    </>
+  );
+}
--- src/components/common/PostUpdate/index.jsx
+++ src/components/common/PostUpdate/index.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, {useState} from "react";
 import "./index.scss";
+import ModalComponent from "../Modal";
 
 export default function PostStatus() {
+    const [open, setOpen] = useState(false);
   return (
     <div className="post-status-main">
-      <div className="post-status"><button className="open-post-modal">Start a post</button></div>
+      <div className="post-status"><button onClick={()=> setOpen(true)} className="open-post-modal">Start a post</button></div>
+      <ModalComponent open={open} setOpen={setOpen} />
     </div>
   );
 }
